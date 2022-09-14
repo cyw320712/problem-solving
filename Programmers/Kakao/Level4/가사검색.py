b870dd998c9a5043de700e2b66f2d00f2590c320
Yongwoo Choi <cyw7515@naver.com>
2022-09-15 00:36:23 +0900

    Add: 가사검색, 프로그래머스, Trie

--- Programmers/Kakao/Level4/가사검색.py
+++ Programmers/Kakao/Level4/가사검색.py
@@ -1,75 +1,43 @@
-from collections import deque
+class Trie:
+	def __init__(self):
+		self.front_root = {'len': []}
+		self.back_root = {'len': []}
+	
+	def insert(self, s, front):
+		cur_node = self.front_root if front else self.back_root
+		cur_node['len'].append(len(s))
 
-def insert(s, root, mode):
-  cur_node = root
-  if mode:
-    for i in range(len(s)):
-      token = s[i]
-      if token not in cur_node:
-        cur_node[token] = {}
-      else:
-        cur_node[token][1] += 1
-      cur_node = cur_node[token]
-    cur_node["*"] = [s]
-  else:
-    for i in reversed(range(len(s))):
-      token = s[i]
-      if token not in cur_node:
-        cur_node[token] = [{}, 1]
-      else:
-        cur_node[token][1] += 1
-      cur_node = cur_node[token]
-    cur_node["*"] = [s]
+		for c in s:
+			if c not in cur_node:
+				cur_node[c] = {'len': []}
+			cur_node = cur_node[c]
+			cur_node['len'].append(len(s))
+		
+		cur_node['len'].append(len(s))
+	
+	def search(self, s, front):
+		cur_node = self.front_root if front else self.back_root
+		s = s if front else s[::-1]
 
-def front_search(s, root):
-  cur_node = root
-  count = 0
-  for i in range(len(s)):
-    token = s[i]
-    if token != "?":
-      if token in cur_node:
-        cur_node = cur_node[token]
-      else:
-        return 0
-    else:
-      count += cur_node[1]
-      break
-  return count
+		for c in s:
+			if c == '?':
+				return cur_node['len'].count(len(s))
+			elif c in cur_node:
+				cur_node = cur_node[c]
+			else:
+				break
 
-def back_search(s, root):
-  cur_node = root
-  count = 0
-  for i in reversed(range(len(s))):
-    token = s[i]
-    if token != "?":
-      if token in cur_node:
-        cur_node = cur_node[token]
-      else:
-        return 0
-    else:
-      count += cur_node[1]
-      break
-  return count
 
 def solution(words, queries):
-  answer = []
+	answer = []
+	wordTree = Trie()
 
-  front_trie = {}
-  back_trie = {}
-  length = []
+	for word in words:
+		wordTree.insert(word, True)
+		wordTree.insert(word[::-1], False)
+	
+	for query in queries:
+		result = wordTree.search(query, query[0] != '?')
+		answer.append(result if result else 0)
 
-  for word in words:
-    insert(word, front_trie, length, True)
-    insert(word, back_trie, length, False)
-  
-  for query in queries:
-    if query[-1] == "?":
-      answer.append(front_search(query, front_trie))
-    elif query[0] == "?":
-      answer.append(back_search(query, back_trie))
-
-  return answer
-
-words = ["frodo", "frozz", "frozz", "frozen", "frame", "kakao"]
-queries = ["fro??"]
-print(solution(words, queries))
+	return answer
