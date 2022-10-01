255cf9530807a984d08e1886e36c582b093ef888
Yongwoo Choi <cyw7515@naver.com>
2022-10-01 22:39:37 +0900

    Refactor: 2629 설명추가 (백준, 양팔저울, dp)

--- Baekjoon/python/2629.py
+++ Baekjoon/python/2629.py
@@ -1,6 +1,5 @@
 import sys
 
-
 standard_num, standard = int(input()), list(map(int, sys.stdin.readline().split()))
 weight_num, weight = int(input()), list(map(int, sys.stdin.readline().split()))
 
@@ -9,19 +8,22 @@ dp[0] = 1
 result = []
 
 for i in range(standard_num):
-    temp = [0 for _ in range(40001)]
+    # 모든 추에 대해서 한 번씩 넣고 빼기를 해서 가능한 것들을 저장
 
-    for j in range(40000, -1, -1):
+    for j in range(0, 40001):
         if dp[j]:
-            temp[j] = 1
-            temp[j + standard[i]] = 1
+            dp[j] = 1
 
+            # 현재 추 더하기
+            dp[j + standard[i]] = 1
+
+            # 현재 추에서 빼기
             if standard[i] - j >= 0:
-                temp[standard[i] - j] = 1
+                dp[standard[i] - j] = 1
+
+            # 현재 가능 무게에서 현재 추 빼기
             elif j - standard[i] >= 0:
-                temp[j - standard[i]] = 1
-    
-    dp = temp.copy()
+                dp[j - standard[i]] = 1
 
 for w in weight:
     if dp[w]:
