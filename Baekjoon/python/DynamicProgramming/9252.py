3b25d0b767e04218c73ff44cc2701b48151f3557
Yongwoo Choi <cyw7515@naver.com>
2022-11-12 20:50:12 +0900

    Fix: 9252.py 메모리 초과 수정

--- Baekjoon/python/DynamicProgramming/9252.py
+++ Baekjoon/python/DynamicProgramming/9252.py
@@ -1,13 +1,13 @@
 str1, str2 = input(), input()
 len1, len2 = len(str1), len(str2)
 
-dp = [[[0, []] for _ in range(len2 + 1)] for _ in range(len1+1)]
+dp = [[[0, ""] for _ in range(len2 + 1)] for _ in range(len1+1)]
 
 for i in range(1, len1+1):
     for j in range(1, len2+1):
         if str1[i-1] == str2[j-1]:
             dp[i][j][0] = dp[i-1][j-1][0] + 1
-            dp[i][j][1] = dp[i-1][j-1][1] + [str1[i-1]]
+            dp[i][j][1] = dp[i-1][j-1][1] + str1[i-1]
         else:
             if dp[i-1][j][0] > dp[i][j-1][0]:
                 dp[i][j][0] = dp[i-1][j][0]
