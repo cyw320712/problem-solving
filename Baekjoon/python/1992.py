063c41b6ed44b2b2fcfb3d2f3139364e97f5537b
Yongwoo Choi <cyw7515@naver.com>
2022-12-03 21:33:13 +0900

    Add: 1992.py (백준, 쿼드트리, 재귀)

---
+++ Baekjoon/python/1992.py
@@ -0,0 +1,26 @@
+n = int(input())
+board = [list(map(int, input())) for _ in range(n)]
+
+def dnc(x, y, n):
+    check = board[x][y]
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if check != board[i][j]:
+                check = -1
+                break
+
+    if check == -1:
+        print("(", end='')
+        n = n // 2
+        dnc(x, y, n)  # 오른쪽 위
+        dnc(x, y + n, n)  # 왼쪽 위
+        dnc(x + n, y, n)  # 오른쪽 아래
+        dnc(x + n, y + n, n)  # 왼쪽 아래
+        print(")", end='')
+
+    elif check == 1:
+        print(1, end='')
+    else:
+        print(0, end='')
+
+dnc(0, 0, n)
